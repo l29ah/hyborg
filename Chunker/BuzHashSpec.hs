7fb99a3815c05e3f09460adc41017aac8de21249
Sergey Alirzaev <zl29ah@gmail.com>
2020-07-26 20:01:37 +0300

    + buzhash test

---
+++ Chunker/BuzHashSpec.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Chunker.BuzHashSpec (spec) where
+
+import Test.Hspec
+
+import Chunker.BuzHash
+
+spec :: Spec
+spec = do
+	describe "buzhash" $ do
+		it "passes borgbackup's test suite" $ do
+			buzhash borgLookupTable "abcdefghijklmnop" `shouldBe` 3795437769
