fcbba695ea03fa8367a0d3352b82a577c21cb9bd
Sergey Alirzaev <zl29ah@gmail.com>
2020-07-27 17:29:43 +0300

    + threaded buzhash benchmark

--- Benchmark.hs
+++ Benchmark.hs
@@ -6,16 +6,19 @@ import qualified Data.ByteString.Lazy as BL
 
 import Chunker.BuzHash
 
+threadify threads computation = parMap rdeepseq computation . replicate threads
+
 byteString1M	= BL.take 1000000 $ BL.iterate (+ 1) 0
 byteString10M	= BL.take 10000000 $ BL.iterate (+ 1) 0
 
 main = defaultMain
 	[ bgroup "buzhash"
-		[ bench "1" $ nf (buzhash borgLookupTable) byteString10M
+		[ bench "10MB" $ nf (buzhash borgLookupTable) byteString10M
+		, bench "5x10MB via 5 threads" $ nf (threadify 5 $ buzhash borgLookupTable) byteString10M
 		]
 	, bgroup "chunkify"
 		[ bench "default settings 1MB" $ nf (chunkify 0 19 23 21 4095) byteString1M
 		, bench "default settings 10MB" $ nf (chunkify 0 19 23 21 4095) byteString10M
-		, bench "default settings 5x10MB via 5 threads" $ nf (parMap rdeepseq (BL.length . chunkify 0 19 23 21 4095)) $ replicate 5 byteString10M
+		, bench "default settings 5x10MB via 5 threads" $ nf (threadify 5 $ BL.length . chunkify 0 19 23 21 4095) byteString10M
 		]
 	]
