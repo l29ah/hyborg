a5718b2a0ee733e2a56e91a32d5edacdc9121f19
Sergey Alirzaev <zl29ah@gmail.com>
2020-07-27 03:50:37 +0300

    benchmark the chunker

--- Benchmark.hs
+++ Benchmark.hs
@@ -5,10 +5,15 @@ import qualified Data.ByteString.Lazy as BL
 
 import Chunker.BuzHash
 
-megabyteByteString = BL.take 1000000 $ BL.iterate (+ 1) 0
+byteString1M	= BL.take 1000000 $ BL.iterate (+ 1) 0
+byteString10M	= BL.take 10000000 $ BL.iterate (+ 1) 0
 
 main = defaultMain
 	[ bgroup "buzhash"
-		[ bench "1" $ nf (buzhash borgLookupTable) megabyteByteString
+		[ bench "1" $ nf (buzhash borgLookupTable) byteString10M
+		]
+	, bgroup "chunkify"
+		[ bench "default settings 1MB" $ nf (chunkify 0 19 23 21 4095) byteString1M
+		, bench "default settings 10MB" $ nf (chunkify 0 19 23 21 4095) byteString10M
 		]
 	]
