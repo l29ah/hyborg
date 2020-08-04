201121f55ec903d2cad247d83122143d7f89d36a
Sergey Alirzaev <zl29ah@gmail.com>
2020-08-04 18:55:42 +0300

    + sketchy borg-compatible command line option parsing

--- Main.hs
+++ Main.hs
@@ -5,11 +5,38 @@ import Data.Binary.Get
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import Data.Maybe
+import Options.Applicative
 
 import Object
 import RPC
 
+data Command =
+	Create
+		{ cProgress :: Bool
+		}
+	| Info
+	deriving (Eq, Show)
+
+data Options = Options
+	{ oVerbose :: Bool
+	, oCommand :: Command
+	} deriving (Eq, Show)
+
+optParser :: Parser Options
+optParser = Options
+	<$> switch (short 'v' <> long "verbose" <> long "info")
+	<*> commandParser
+
+commandParser :: Parser Command
+commandParser = hsubparser $
+	(command "info" $ info (pure Info) (progDesc "show repo info"))
+	<> (command "create" $ info (Create
+		<$> switch (long "progress")
+		) (progDesc "create a new backup"))
+
 main = do
+	opts <- execParser $ info optParser (progDesc "borgbackup-compatible backup tool")
+	print opts
 	conn <- openRPC
 	negotiate conn
 	open conn "/home/l29ah/projects/hyborg/test" True
